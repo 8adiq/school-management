1b280fd79dd9dcc2b98166abf310016fe6aed429
8adiq <abdul.latif708@yahoo.com>
2024-08-28 10:34:07 +0000

    updated gitignore file

--- app/dp.py
+++ app/dp.py
@@ -1,17 +1,48 @@
-import psycopg2
+import psycopg2, os
+from flask import jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # connecting to database
 def connect_to_db():
     """connecting to database"""
     try:
         connection = psycopg2.connect(
-            dbname="school_db",
-            user="postgres",
-            password="admin",
-            host="localhost",
-            port="5432"
+            dbname=os.getenv("dbname"),
+            user=os.getenv("user"),
+            password=os.getenv("password"),
+            host=os.getenv("host"),
+            port=os.getenv("port")
         )
         print('Database connected successfully.')
         return connection
     except (Exception,psycopg2.Error) as e:
         print(f'The error {e} occured.')
+
+
+def execute_query(query,params=None):
+    """"""
+    cur = None
+    conn = None
+
+    try:
+        conn = connect_to_db()
+
+        if not conn:
+            return jsonify({'error':'Failed to connect to database'})
+        
+        cur = conn.cursor()
+        cur.execute(query,params)
+        conn.commit()
+        return cur
+    
+    except (Exception,psycopg2.Error) as e:
+        return jsonify({'error':str(e)})
+    
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+        
