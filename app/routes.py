628e6d448139210738b9a37351fdcce38f1d1cc9
8adiq <abdul.latif708@yahoo.com>
2024-09-05 16:14:07 +0000

    imported and used flask migrate

--- app/routes.py
+++ app/routes.py
@@ -1,150 +1,145 @@
 import psycopg2
-from flask import request,jsonify,Blueprint
-from .dp import connect_to_db,execute_query
-from .utils import student_to_dict
+from flask import  request,jsonify
+from sqlalchemy.exc import SQLAlchemyError
+from models import Student
+# from app import db
 
-main_bp = Blueprint('main',__name__)
-
+def all_routes(app,db):
 # interacting with the database to perform CRUD operation using API routes
-@main_bp.route('/add-student', methods =['POST'])
-def add_student():
-    """adding a new student to the database"""
-    try:
-        # extracting the data parsed from the body of the request.
-        data = request.get_json()
-        name = data['name']
-        age = data['age']
-        grade = data['grade']
+    @app.route('/add-student', methods =['POST'])
+    def add_student():
+        """adding a new student to the database"""
+        try:
+            # extracting the data parsed from the body of the request.
+            data = request.get_json()
+            name = data['name']
+            age = data['age']
+            grade = data['grade']
 
-        # making sure all variables are provided.
-        if not name or not age or not grade:
-            return jsonify({'error': 'name, age and grade required.'}),400
+            # making sure all variables are provided.
+            if any([
+                not name,
+                not age,
+                not grade
+            ]):
+                return jsonify({'error': 'name, age and grade required.'}),400
+            
+            # data type validation
+            if any([
+                not isinstance(name,str),
+                not isinstance(age,int),
+                not isinstance(grade,str)
+            ]):
+                return jsonify({'Error':'Please enter the data in their correct data type'})
+            
+            # creating a student as an object of the model
+            student = Student(name=name,age=age,grade=grade)
+
+            # adding student to db
+            db.session.add(student)
+            db.session.commit()
+
+            return jsonify({'message':f'{name} has been added.'}),201
         
-        query = 'INSERT INTO students_1 (student_name,age,grade) VALUES (%s,%s,%s)'
-        execute_query(query,(name,age,grade))
+        except (Exception,psycopg2.Error) as e:
+            return jsonify({'error': str(e)}), 500
 
-        return jsonify({'message':f'{name} has been added.'}),201
-    
-    except (Exception,psycopg2.Error) as e:
-         return jsonify({'error': str(e)}), 500
+    @app.route('/update-student', methods =['PUT'])
+    def upgrade_student():
+        """upgrading student data"""
 
-@main_bp.route('/update-student', methods =['PUT'])
-def upgrade_student():
-    """upgrading student data"""
+        try:
+            data = request.get_json()
+            student_id = data.get('student_id')
+            
+            if not student_id:
+                return jsonify({'error':'Student ID is required'}),400
 
-    try:
-        data = request.get_json()
-        name = data['name']
-        student_id = data['student_id']
-        age = data['age']
-        grade = data['grade']
+            student = Student.query.get_or_404(student_id)
 
-        if not student_id:
-            return jsonify({'error':'Student ID is required'}),400
-        if not name or not age or not grade:
-            return jsonify({'error': 'At least one of name, age or grade required.'}),400
+            name = data.get('name')
+            age = data.get('age')
+            grade = data.get('grade')
 
-        update_fields = []
-        params = []
+            if not any([name,age,grade]): 
+                return jsonify({'error': 'at least one of name, age or grade required.'}),400
+            
+            # checking which variable was provided and uodating it.
+            if name is not None:
+                student.name = name
+            if age is not None:
+                student.age = age
+            if grade is not None:
+                student.grade = grade
 
-        # checking which variable was provided
-        if name:
-            update_fields.append('student_name = %s')
-            params.append(name)
-        if age:
-            update_fields.append('age = %s')
-            params.append(age)
-        if grade:
-            update_fields.append('grade = %s')
-            params.append(grade)
+            db.session.commit()
+            
+            return jsonify({'message':'Student updated successfully'})
         
-        if not update_fields:
-            return jsonify({'error':'No fields to update'}),400
+        except (Exception,psycopg2.Error) as e:
+            return jsonify({'error':str(e)}),500
         
-        # connecting to DB and creating dynamic query based on which variable was provided
-        query = 'UPDATE students_1 SET ' + ', '.join(update_fields) + ' WHERE student_id=%s' 
-        params.append(student_id)
 
-        cur = execute_query(query,params)
-        if cur.rowcount == 0:
-            return jsonify({'error':f'no student with the name {name}'})
+    @app.route('/display-student/<int:student_id>', methods =['GET'])
+    def display_student(student_id):
+        """displaying student infomation"""
+
+        try:
+
+            student = Student.query.filter(Student.sid==student_id).first()
+            if student is None:
+                return jsonify({'error':'No student with that ID'}),404
+            else:
+                student_dict = {
+                    'name' : student.name,
+                    'age': student.age,
+                    'grade': student.grade
+                }
+            
+            return jsonify({'Students': student_dict}),200
         
-        return jsonify({'message':'Student updated successfully'})
-    
-    except (Exception,psycopg2.Error) as e:
-        return jsonify({'error':str(e)}),500
-    
+        except (Exception,psycopg2.Error) as e:
+            return jsonify({'error',str(e)}),500
 
-@main_bp.route('/display-student/<int:student_id>', methods =['GET'])
-def display_student(student_id):
-    """displaying student infomation"""
+    @app.route('/display-students/', methods =['GET'])
+    def display_students():
+        """displaying all students information"""
 
-    try:
-        conn = connect_to_db()
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM students_1 WHERE student_id = %s',(student_id,))
-        student = cur.fetchone()
-
-        if student is None:
-            return jsonify({'error':'No student with that ID'}),404
+        try:
+            # connecting to database and fetching all students data
+            students = Student.query.all()
+            
+            if not students:
+                return jsonify({'Message':'Database is empty'})
+            # creating a list of student dictionaries to return as JSON
+            students_list = []
+            for student in students:
+                student_dict ={
+                    'name': student.name,
+                    'age': student.age,
+                    'grade':student.grade
+                }
+                students_list.append(student_dict)
+            return jsonify({'Students':students_list}),200
         
-        student_dict =student_to_dict(student)
-        return jsonify({'Students': student_dict}),200
-    
-    except (Exception,psycopg2.Error) as e:
-        return jsonify({'error',str(e)}),500
-    
-    finally:
-        if cur is not None:
-            cur.close()
-        if conn is not None:
-            conn.close()
+        except (Exception,psycopg2.Error) as e:
+            return jsonify({'error': str(e)}),500
+        
+    @app.route('/remove-student/', methods =['POST'])
+    def remove_student():
+        """deleting a student from the database"""
 
-@main_bp.route('/display-students/', methods =['GET'])
-def display_students():
-    """displaying all students information"""
-     
-    conn = None
-    cur = None
-    try:
-        # connecting to database and fetching all students data
-        conn = connect_to_db()
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM students_1')
-        students = cur.fetchall()
+        try:   
+            data = request.get_json()
+            student_id = data.get('student_id')
 
-        # creating a list of student dictionaries to return as JSON
-        students_list = []
-        for student in students:
-            student_dict = student_to_dict(student)
-            students_list.append(student_dict)
-        return jsonify({'Students':students_list}),200
-    
-    except (Exception,psycopg2.Error) as e:
-        return jsonify({'error': str(e)}),500
-    
-    finally:
-        if cur is not None:
-            cur.close()
-        if conn is not None:
-            conn.close()
-
-@main_bp.route('/remove-student/<int:student_id>', methods =['DELETE'])
-def remove_student(student_id):
-    """deleting a student from the database"""
-
-    try:
-        conn = connect_to_db()
-        cur = conn.cursor()
-        cur.execute('DELETE FROM students_1 where student_id = %s',(student_id,))
-        conn.commit()
-        return jsonify({'message':' student deleted'})
-    
-    except (Exception,psycopg2.Error) as e:
-        return jsonify({'error': str(e)}),500
-    
-    finally:
-        if cur is not None:
-            cur.close()
-        if conn is not None:
-            conn.close()
+            student = Student.query.filter(Student.sid == student_id).first()
+            if student:
+                db.session.delete(student)
+                db.session.commit()
+                return jsonify({'message':' student deleted'}),200
+            else:
+                return jsonify({'message': 'Student not found'}), 404
+        
+        except SQLAlchemyError as e:
+            return jsonify({'error': str(e)}),500
